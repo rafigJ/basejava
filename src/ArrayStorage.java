d8a0f340901c7c3399a37b90c7a55134bf257847
Rafig <rafigdzabbarov0410@gmail.com>
2023-05-19 02:08:08 +0300

    First commit

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,50 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; storage[i] != null; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size()] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; storage[i] != null; i++) {
+            if (storage[i].toString().equals(uuid))
+                return storage[i];
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int remove_index = size();
+        for (int i = 0; storage[i] != null; i++) {
+            if (storage[i].toString().equals(uuid)){
+                remove_index = i;
+                break;
+            }
+        }
+        for (int i = remove_index; i < size(); i++) {
+            storage[i] = storage[i + 1];
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        if (size() >= 0) System.arraycopy(storage, 0, resumes, 0, size());
+        return resumes;
     }
 
     int size() {
-        return 0;
+        int i = 0;
+        while (storage[i] != null){
+            i++;
+        }
+        return i;
     }
 }
