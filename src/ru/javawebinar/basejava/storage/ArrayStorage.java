d5817c936278cd895ad5296d94821e5d4ff28c9b
Rafig <rafigdzabbarov0410@gmail.com>
2023-06-13 12:53:15 +0300

    Fixed clear, update, save... methods

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -8,41 +8,40 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size;
 
     public void clear() {
-        Arrays.fill(storage, 0, size - 1, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public void update(Resume r) {
-        int indexForUpdate = getIndexResumeByUuid(r.getUuid());
-        if (indexForUpdate == -1) {
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
             System.out.printf("ERROR: a resume with a similar uuid is not present in the storage; uuid: %s%n", r.getUuid());
             return;
         }
-        storage[indexForUpdate] = r;
+        storage[index] = r;
     }
 
     public void save(Resume r) {
-        if (resumeIsPresent(r)) {
-            System.out.printf("ERROR: a resume with a similar uuid is present in the storage; uuid: %s%n", r.getUuid());
-            return;
-        }
-        if (size >= storage.length) {
+        if (size >= STORAGE_LIMIT) {
             System.out.printf("ERROR: the storage is overflowing with uuid %s%n", r.getUuid());
-            return;
+        } else if (isExist(r.getUuid())) {
+            System.out.printf("ERROR: a resume with a similar uuid is present in the storage; uuid: %s%n", r.getUuid());
+        } else {
+            storage[size] = r;
+            size++;
         }
-        storage[size] = r;
-        size++;
     }
 
-    private boolean resumeIsPresent(Resume r) {
-        return getIndexResumeByUuid(r.getUuid()) != -1;
+    private boolean isExist(String uuid) {
+        return getIndex(uuid) != -1;
     }
 
-    private int getIndexResumeByUuid(String uuid) {
+    private int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
@@ -52,19 +51,22 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int index = getIndexResumeByUuid(uuid);
-        return index != -1 ? storage[index] : null;
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.printf("ERROR: A resume with the given uuid is not in storage. uuid: %s%n", uuid);
+            return null;
+        }
+        return storage[index];
     }
 
     public void delete(String uuid) {
-        int indexForRemove = getIndexResumeByUuid(uuid);
-        if (indexForRemove == -1) {
-            System.out.printf("ERROR: the resume with the entered uuid is not present in the storage; uuid: %s%n", uuid);
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.printf("ERROR: the resume with the entered uuid is not present in the storage. uuid: %s%n", uuid);
             return;
         }
-        for (int i = indexForRemove; i < size; i++) {
-            storage[i] = storage[i + 1];
-        }
+        storage[index] = storage[size - 1];
+        storage[size - 1] = null;
         size--;
     }
 
