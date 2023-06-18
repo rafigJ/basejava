ce6ce49ff9975c3fd41742d8f2c6e1487d1ae0a7
Rafig <rafigdzabbarov0410@gmail.com>
2023-06-18 16:03:45 +0300

    + Template Methods save, delete

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -9,30 +9,6 @@ public abstract class AbstractArrayStorage implements Storage {
     protected final Resume[] storage = new Resume[STORAGE_LIMIT];
     protected int size;
 
-    public final void save(Resume r){
-        int index = getIndex(r.getUuid());
-        if (size == STORAGE_LIMIT) {
-            System.out.printf("ERROR: the storage is overflowing with uuid %s%n", r.getUuid());
-        } else if (isExist(index)) {
-            System.out.printf("ERROR: a resume with a similar uuid is present in the storage; uuid: %s%n", r.getUuid());
-        } else {
-            doSave(r, index);
-        }
-    }
-
-    protected abstract void doSave(Resume r, int index);
-
-    public final void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (!isExist(index)) {
-            System.out.printf("ERROR: the resume with the entered uuid is not present in the storage. uuid: %s%n", uuid);
-        } else {
-            doDelete(index);
-        }
-    }
-
-    protected abstract void doDelete(int index);
-
     public final void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
@@ -47,9 +23,18 @@ public abstract class AbstractArrayStorage implements Storage {
         storage[index] = r;
     }
 
-    public final int size() {
-        return size;
+    public final void save(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (size == STORAGE_LIMIT) {
+            System.out.printf("ERROR: the storage is overflowing with uuid %s%n", r.getUuid());
+        } else if (isExist(index)) {
+            System.out.printf("ERROR: a resume with a similar uuid is present in the storage; uuid: %s%n", r.getUuid());
+        } else {
+            doSave(r, index);
+        }
     }
+
+    protected abstract void doSave(Resume r, int index);
 
     public final Resume get(String uuid) {
         int index = getIndex(uuid);
@@ -60,13 +45,28 @@ public abstract class AbstractArrayStorage implements Storage {
         return storage[index];
     }
 
+    public final void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (!isExist(index)) {
+            System.out.printf("ERROR: the resume with the entered uuid is not present in the storage. uuid: %s%n", uuid);
+        } else {
+            doDelete(index);
+        }
+    }
+
+    protected abstract void doDelete(int index);
+
     public final Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
     }
 
+    public final int size() {
+        return size;
+    }
+
     protected abstract int getIndex(String uuid);
 
-    protected boolean isExist(int index) {
+    protected final boolean isExist(int index) {
         return index > -1;
     }
 }
