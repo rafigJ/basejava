24370c8a3e26292cfa416b8f266bbed7707c95b8
Rafig <rafigdzabbarov0410@gmail.com>
2023-07-07 17:38:59 +0300

    HW6_pre

--- test/ru/javawebinar/basejava/storage/AllTestRunner.java
+++ test/ru/javawebinar/basejava/storage/AllTestRunner.java
@@ -4,6 +4,6 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({ArrayStorageTest.class, SortedArrayStorageTest.class, ListStorageTest.class, MapUuidStorageTest.class})
+@SelectClasses({ArrayStorageTest.class, SortedArrayStorageTest.class, ListStorageTest.class, MapUuidStorageTest.class, MapResumeStorageTest.class})
 public class AllTestRunner {
 }
