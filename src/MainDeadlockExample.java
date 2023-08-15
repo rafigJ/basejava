df5ff78652d4352ded6cb966de035a0cbb399c78
Rafig <rafigdzabbarov0410@gmail.com>
2023-08-15 20:08:02 +0300

    Deadlock

---
+++ src/MainDeadlockExample.java
@@ -0,0 +1,46 @@
+public class MainDeadlockExample {
+    public static class Class1 {
+        public synchronized Class2 getClass2(Class2 class2) {
+            String name = Thread.currentThread().getName();
+            System.out.println(name + " working in getClass2");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(name + " try getName class2");
+            String s = class2.getName();
+            return class2;
+        }
+
+        public synchronized String getName(){
+            return this.getName() + " Class1";
+        }
+    }
+
+    public static class Class2 {
+        public synchronized Class1 getClass1(Class1 class1) {
+            String name = Thread.currentThread().getName();
+            System.out.println(name + " working in getClass2");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(name + " try getName class1");
+            String s = class1.getName();
+            return class1;
+        }
+
+        public synchronized String getName(){
+            return this.getName() + " Class2";
+        }
+    }
+
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        Class2 class2 = new Class2();
+        new Thread(() -> class1.getClass2(class2)).start();
+        new Thread(() -> class2.getClass1(class1)).start();
+    }
+}
--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -7,66 +7,39 @@ public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private int counter;
     private static final Object LOCK = new Object();
+    private static final Object LOCK2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(Thread.currentThread().getName());
-
-        Thread thread0 = new Thread() {
-            @Override
-            public void run() {
-                System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
-            }
-        };
-        thread0.start();
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
-            }
-
-            private void inc() {
-                synchronized (this) {
-//                    counter++;
-                }
-            }
-
-        }).start();
-
-        System.out.println(thread0.getState());
-
+//        System.out.println(Thread.currentThread().getName());
+//
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
-
+        List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < THREADS_NUMBER; i++) {
             Thread thread = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
                 }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             });
             thread.start();
             threads.add(thread);
+            System.out.println("ACTIVE COUNT THREADS: " + Thread.activeCount());
         }
-
-        threads.forEach(t -> {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        for (Thread t : threads) {
+            t.join();
+        }
         System.out.println(mainConcurrency.counter);
+
     }
 
     private synchronized void inc() {
 //        synchronized (this) {
 //        synchronized (MainConcurrency.class) {
+//        System.out.println(Thread.currentThread().getName());
         counter++;
-//                wait();
-//                readFile
-//                ...
-//        }
     }
 }
