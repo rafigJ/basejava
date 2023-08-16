303c6b4a17afd34a35a0547aa73d751133c1c27e
Rafig <rafigdzabbarov0410@gmail.com>
2023-08-16 18:58:18 +0300

    Deadlock Fix

--- src/MainDeadlockExample.java
+++ src/MainDeadlockExample.java
@@ -1,8 +1,8 @@
 public class MainDeadlockExample {
-    private static class Class1 {
-        public synchronized Class2 getClass2(Class2 class2) {
+    private static class DeadClass {
+        public synchronized DeadClass getDead(DeadClass class2) {
             String name = Thread.currentThread().getName();
-            System.out.println(name + " working in getClass2");
+            System.out.println(name + " working in getDead");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -18,29 +18,12 @@ public class MainDeadlockExample {
         }
     }
 
-    private static class Class2 {
-        public synchronized Class1 getClass1(Class1 class1) {
-            String name = Thread.currentThread().getName();
-            System.out.println(name + " working in getClass1");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println(name + " try getName class1");
-            String s = class1.getName();
-            return class1;
-        }
-
-        public synchronized String getName(){
-            return this.toString();
-        }
-    }
 
     public static void main(String[] args) {
-        Class1 class1 = new Class1();
-        Class2 class2 = new Class2();
-        new Thread(() -> class1.getClass2(class2)).start();
-        new Thread(() -> class2.getClass1(class1)).start();
+        DeadClass class1 = new DeadClass();
+        DeadClass class2 = new DeadClass();
+
+        new Thread(() -> class1.getDead(class2)).start();
+        new Thread(() -> class2.getDead(class1)).start();
     }
 }
