8a54668c2dc57611579c81a86fd8a586545615d4
Rafig <rafigdzabbarov0410@gmail.com>
2023-09-23 14:09:40 +0300

    Lesson 15

---
+++ src/ru/javawebinar/basejava/ResumeServlet.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.basejava;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello!!" : "Hello, " + name);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
